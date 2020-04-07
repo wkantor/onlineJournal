08bafd052eb64cf3a638cbf116265330836c8241
Vojtech Kantor <wkantor@gmail.com>
2020-04-07 13:31:35 +0200

    Test 2
    Second test of security stuff

--- Journal/settings.py
+++ Journal/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('APP_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split()
 
 
 
