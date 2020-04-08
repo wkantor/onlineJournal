1ee14166da14a22f920b03832aeeb113eb98d0ec
Vojtech Kantor <wkantor@gmail.com>
2020-04-08 08:30:20 +0200

    reCaptcha
    captcha added, let's see if it works :)

--- Journal/settings.py
+++ Journal/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('APP_SECRET_KEY')
+GOOGLE_RECAPTCHA_SECRET_KEY = os.environ.get('GOOGLE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG')
